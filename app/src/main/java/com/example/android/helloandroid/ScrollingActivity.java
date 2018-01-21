45d2f9144d52a4c3b38d9f7eb5e2d1f6297dc37b
Anand Rudraraju <anandrudraraju@gmail.com>
2018-01-21 00:59:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/helloandroid/ScrollingActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.helloandroid;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+
+}
